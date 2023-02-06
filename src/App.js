36b271c022df195fd213e55fea8c0d680d4bf170
vamsikanaparthi <kanaparthivamsi222@gmail.com>
2023-02-06 08:57:12 +0530

    chaithanya

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Ifelse from './Conditional Rendering Examples/Ifelse'
 import Ternary from './Conditional Rendering Examples/Ternary'
 import EventBinding from './Event Handling Examples/EventBinding'
 import EventHandling from './Event Handling Examples/EventHandling'
+import ListRendering from './List Rendering ex/ListRendering'
+import ListRenderingex2 from './List Rendering ex/ListRenderingex2'
+import Namelist from './List Rendering ex/Namelist'
 import Navbar from './Navbar'
 import Counter from './state/Counter'
 import Message from './state/Message'
 import Products from './state/Products'
+import Login from './Form Handling/Login'
+import Registration from './Form Handling/Registration'
 
 function App() {
   return (
@@ -22,7 +27,12 @@ function App() {
     <EventHandling/><hr/>
     <EventBinding/><hr/>
     <Ifelse/><hr/>
-    <Ternary/>
+    <Ternary/><hr/>
+    <ListRendering/><hr/>
+    <ListRenderingex2/><hr/>
+    <Namelist/><hr/>
+    <Login/><hr/>
+    <Registration/>
     </>
   )
 }
